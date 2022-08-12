794964161e4d8b7981ecd97cd4b551b6f2d926c9
ElenaG <freshelf@yandex.ru>
2022-08-12 11:10:09 +0500

    add Task001

---
+++ Task001/Program.cs
@@ -0,0 +1,19 @@
+﻿
+int FindQuarter(int x, int y)
+{
+    if (x > 0 && y > 0) 
+        return 1;
+    else if (x < 0 && y > 0)
+        return 2;
+    else if (x < 0 && y < 0)
+        return 3;
+    else if (x > 0 && y < 0)
+        return 4;
+    
+    return 0;
+}
+Console.WriteLine(FindQuarter(4, 9));
+Console.WriteLine(FindQuarter(-4, -9));
+Console.WriteLine(FindQuarter(4, -9));
+Console.WriteLine(FindQuarter(-4, 9));
+Console.WriteLine(FindQuarter(4, 0));
