c25f874c3886179823ac5cc1ff79dd1709c1441e
ElenaG <freshelf@yandex.ru>
2022-08-12 11:11:51 +0500

    add Task002

---
+++ Task002/Program.cs
@@ -0,0 +1,20 @@
+﻿void FindRange(int number)
+{
+    if (number == 1) 
+        Console.WriteLine("x > 0, y > 0");
+    else if (number == 2)
+        Console.WriteLine("x < 0, y > 0");
+    else if (number == 3)
+        Console.WriteLine("x < 0, y < 0");
+    else if (number == 4)
+        Console.WriteLine("x > 0, y < 0");
+    else
+        Console.WriteLine("Введите корректный номер четверти");
+}
+FindRange(1);
+FindRange(2);
+FindRange(3);
+FindRange(4);
+FindRange(0);
+FindRange(-4);
+FindRange(12);
