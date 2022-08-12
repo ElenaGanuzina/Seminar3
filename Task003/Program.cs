2d13a69da64c2c40e6546b467ecaf892fdddb7c4
ElenaG <freshelf@yandex.ru>
2022-08-12 11:12:56 +0500

    add Task003

---
+++ Task003/Program.cs
@@ -0,0 +1,8 @@
+﻿double FindDistance(double x, double y, double x1, double y1)
+{
+    double a = x1 -x;
+    double b = y1 - y;
+    double distance = Math.Sqrt(Math.Pow(a, 2) + Math.Pow(b, 2));
+    return distance;
+}
+Console.WriteLine(FindDistance(4, 9, -5, 0));
