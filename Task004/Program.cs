b740cd1a8d3ffb3edb77a0e86149bbb2191f42b1
ElenaG <freshelf@yandex.ru>
2022-08-12 11:14:09 +0500

    add Task004

---
+++ Task004/Program.cs
@@ -0,0 +1,15 @@
+﻿void FindSquares(int number)
+{
+    Console.Write($"{number} -> ");
+
+    int count = 1;
+    while (count < number)
+    {
+        Console.Write($"{count * count}, ");
+        count++;  
+    }
+Console.WriteLine($"{number * number}.");
+}
+
+FindSquares(4);
+FindSquares(7);
